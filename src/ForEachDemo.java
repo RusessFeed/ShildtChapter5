7ade868b9f3ddbbd05ab2ea2c048d07256bd09a1
Ruslan <ruslanmarlinskiy@gmail.com>
2024-06-14 00:44:04 +0300

    This is how foreach works

--- src/ForEachDemo.java
+++ src/ForEachDemo.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ForEachDemo {
+public class ForEachDemo {
+    public static void main(String[] args) {
+        int pepepe[] = {35, 45, 55, 65};
+        int sum = 0;
+
+        for (int popopo : pepepe) {
+            System.out.println("Массив хранит: " + popopo);
+            sum += popopo;
+        }
+        System.out.println("Сумма = " + sum);
+
+
+        String[][] stringArrays = {
+                {"Zelim", "Ilyas", "Rusya", "Ankalaev"},
+                {"Mohammed", "Ahmed", "Evgeniy"}
+        };
+
+        for (String[] stringArray : stringArrays) {
+            for (String name : stringArray) {
+                System.out.println("Имя: " + name);
+            }
+        }
+
+
+
+    }
 }
+
+
+
